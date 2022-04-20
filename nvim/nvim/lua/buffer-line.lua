1c47db0a86ca736598f7522b5e53c1af0dc546b2
Jeff Davis <jeffda@vmware.com>
2022-04-20 00:15:11 -0400

    fix(nvim): exclude nnn browser from buffer line
    Signed-off-by: Jeff Davis <jeffda@vmware.com>

--- nvim/nvim/lua/buffer-line.lua
+++ nvim/nvim/lua/buffer-line.lua
@@ -14,5 +14,10 @@ buffer.setup {
       text_align = "left",
       }
     },
+    custom_filter = function(buf_number, buf_numbers)
+      if vim.bo[buf_number].filetype ~= "nnn" then
+        return true
+      end
+    end
   },
 }
