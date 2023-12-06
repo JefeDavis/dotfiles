ed75d86133ab163a025e68a386c0577dc6d9a076
Jeff Davis <mr.jefedavis@gmail.com>
2023-12-06 16:39:29 -0500

    style(nvim): clean up plugin structure
    Signed-off-by: Jeff Davis <mr.jefedavis@gmail.com>

--- home/dot_config/nvim/lua/plugins/keymap.lua
+++ home/dot_config/nvim/lua/plugins/keymap.lua
@@ -1,12 +1,14 @@
 return {
-  "folke/which-key.nvim",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+  {
+    "folke/which-key.nvim",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
   }
 }
