48eea3fd1a5a8ad89aaba068870b6ee6c7e1e0a4
Jeff Davis <mr.jefedavis@gmail.com>
2024-01-31 22:53:56 -0500

    fix(nvim): allow correct font to be used
    Signed-off-by: Jeff Davis <mr.jefedavis@gmail.com>

--- home/private_dot_config/nvim/lua/plugins/lsp.lua
+++ home/private_dot_config/nvim/lua/plugins/lsp.lua
@@ -66,11 +66,10 @@ return {
 			},
 		})
 
-		-- NOTES: optional icons
-		-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		-- for type, icon in pairs(signs) do
-		--   local hl = "DiagnosticSign" .. type
-		--   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		-- end
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 	end,
 }
