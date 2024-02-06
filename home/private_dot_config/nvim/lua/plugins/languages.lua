d8e5e95d271fe0729fdda7b466d762bc3eee7eb5
Jeff Davis <mr.jefedavis@gmail.com>
2024-02-05 21:59:59 -0500

    feat(nvim): add kubernetes and helm language support
    Signed-off-by: Jeff Davis <mr.jefedavis@gmail.com>

--- home/private_dot_config/nvim/lua/plugins/languages.lua
+++ home/private_dot_config/nvim/lua/plugins/languages.lua
@@ -1,9 +1,9 @@
 return {
-  { import = "lazyvim.plugins.extras.lang.docker" },
-  { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.markdown" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
-  { import = "lazyvim.plugins.extras.lang.terraform" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
+	{ import = "lazyvim.plugins.extras.lang.docker" },
+	{ import = "lazyvim.plugins.extras.lang.go" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.markdown" },
+	{ import = "lazyvim.plugins.extras.lang.rust" },
+	{ import = "lazyvim.plugins.extras.lang.terraform" },
+	{ import = "lazyvim.plugins.extras.lang.yaml" },
 }
---
+++ home/private_dot_config/nvim/lua/plugins/yaml.lua
@@ -0,0 +1,157 @@
+local Util = require("lazyvim.util")
+
+return {
+  -- add yaml specific modules to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "yaml" })
+      end
+    end,
+  },
+
+  -- correctly setup lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "b0o/SchemaStore.nvim",
+      lazy = true,
+      version = false, -- last release is way too old
+    },
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        yamlls = {
+          -- Have to add this for yamlls to understand that we support line folding
+          capabilities = {
+            textDocument = {
+              foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true,
+              },
+            },
+          },
+          -- lazy-load schemastore when needed
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
+          end,
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              validate = true,
+              hover = true,
+              completion = true,
+              schemaStore = {
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+              schemas = {
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+                ["https://kubernetes-schemas.davishaus.dev/kustomize.toolkit.fluxcd.io/kustomization_v1.json"] = "ks.yaml",
+                ["https://kubernetes-schemas.davishaus.dev/helm.toolkit.fluxcd.io/helmrelease_v2beta2.json"] = "helmrelease.yaml",
+                kubernetes = "*.yaml",
+              },
+            },
+          },
+        },
+      },
+      setup = {
+        yamlls = function()
+          -- Neovim < 0.10 does not have dynamic registration for formatting
+          if vim.fn.has("nvim-0.10") == 0 then
+            require("lazyvim.util").lsp.on_attach(function(client, bufnr)
+              if client.name == "yamlls" and vim.bo.filetype == "helm" then
+                vim.lsp.stop_client(bufnr, client.id)
+              end
+              if client.name == "yamlls" then
+                client.server_capabilities.documentFormattingProvider = true
+              end
+            end)
+          end
+        end,
+      },
+    },
+  },
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    ft = "yaml",
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("yaml_schema")
+      local cfg = require("yaml-companion").setup({
+        builtin_matchers = {
+          -- Detects Kubernetes files based on content
+          kubernetes = { enabled = true },
+          cloud_init = { enabled = true },
+        },
+        -- Add any options here, or leave empty to use the default settings
+        lspconfig = {
+          flags = {
+            debounce_text_changes = 150,
+          },
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              validate = true,
+              format = { enable = false },
+              hover = true,
+              schemaStore = {
+                enable = true,
+                url = "https://www.schemastore.org/api/json/catalog.json",
+              },
+              schemaDownload = { enable = true },
+              schemas = {},
+              trace = { server = "debug" },
+            },
+          },
+        },
+        -- Additional schemas available in Telescope picker
+        schemas = {
+          {
+            name = "Flux Kustomize",
+            uri = "https://kubernetes-schemas.davishaus.dev/kustomize.toolkit.fluxcd.io/kustomization_v1.json",
+          },
+          {
+            name = "Flux Helm Release",
+            uri = "https://kubernetes-schemas.davishaus.dev/helm.toolkit.fluxcd.io/helmrelease_v2beta2.json",
+          },
+        },
+      })
+      require("lspconfig")["yamlls"].setup(cfg)
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    ft = "yaml",
+    keys = {
+      { "<leader>cs", "<CMD>Telescope yaml_schema<CR>", desc = "Set Yaml Schema" },
+    },
+  },
+}
