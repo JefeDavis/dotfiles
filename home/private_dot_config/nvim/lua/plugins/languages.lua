d8fa126fa3050f2470ae54dec7dfcbdf23bd3d7f
Jeff Davis <mr.jefedavis@gmail.com>
2024-02-12 13:03:33 -0500

    feat(nvim): open Markdown preview in incognito
    Signed-off-by: Jeff Davis <mr.jefedavis@gmail.com>

--- home/private_dot_config/nvim/lua/plugins/languages.lua
+++ home/private_dot_config/nvim/lua/plugins/languages.lua
@@ -6,4 +6,19 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.rust" },
 	{ import = "lazyvim.plugins.extras.lang.terraform" },
 	{ import = "lazyvim.plugins.extras.lang.yaml" },
+	{
+		"iamcco/markdown-preview.nvim",
+		opts = function()
+			vim.api.nvim_exec(
+				[[
+        function! Open_preview_in_incognito_window(url)
+          silent !clear
+          execute "silent !open -n -a '/Applications/Google\ Chrome.app' --args -incognito " . a:url
+        endfunction
+        let g:mkdp_browserfunc = 'Open_preview_in_incognito_window'
+      ]],
+				false
+			)
+		end,
+	},
 }
