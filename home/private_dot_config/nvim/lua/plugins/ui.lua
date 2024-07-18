aa2609d3ea5fdf0b92d2e1bfbf7e4488495748ee
Jeff Davis <mr.jefedavis@gmail.com>
2024-07-18 13:04:23 -0400

    feat: update config
    Signed-off-by: Jeff Davis <mr.jefedavis@gmail.com>

--- home/private_dot_config/nvim/lua/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/plugins/ui.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    opts = function()
-      local logo = [[
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		opts = function()
+			local logo = [[
      ██╗███████╗███████╗███████╗██████╗  █████╗ ██╗   ██╗██╗███████╗
      ██║██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗██║   ██║██║██╔════╝
      ██║█████╗  █████╗  █████╗  ██║  ██║███████║██║   ██║██║███████╗
@@ -12,17 +12,17 @@ return {
  ╚════╝ ╚══════╝╚═╝     ╚══════╝╚═════╝ ╚═╝  ╚═╝  ╚═══╝  ╚═╝╚══════╝
       ]]
 
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-      local opts = {
-        theme = "doom",
-        hide = {
-          -- this is taken care of by lualine
-          -- enabling this messes up the actual laststatus setting after loading a file
-          statusline = false,
-        },
-        config = {
-          header = vim.split(logo, "\n"),
+			local opts = {
+				theme = "doom",
+				hide = {
+					-- this is taken care of by lualine
+					-- enabling this messes up the actual laststatus setting after loading a file
+					statusline = false,
+				},
+				config = {
+					header = vim.split(logo, "\n"),
           -- stylua: ignore
           center = {
             { action = "Telescope find_files",                                     desc = " Find file",       icon = " ", key = "f" },
@@ -36,54 +36,56 @@ return {
             { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
             { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
           },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
-        },
-      }
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						return {
+							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+						}
+					end,
+				},
+			}
 
-      for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
-      end
+			for _, button in ipairs(opts.config.center) do
+				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+				button.key_format = "  %s"
+			end
 
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "DashboardLoaded",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
+			-- close Lazy and re-open when the dashboard is ready
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "DashboardLoaded",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
 
-      return opts
-    end,
-  },
-  {
-    "folke/zen-mode.nvim",
-    keys = {
-      { "<leader>uz", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
-    },
-  },
-  {
-    "folke/twilight.nvim",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-  -- Modify which-key keys
-  {
-    "folke/which-key.nvim",
-    opts = function()
-      require("which-key").register({
-        ["<leader>z"] = { name = "+fold" },
-      })
-    end,
-  },
+			return opts
+		end,
+	},
+	{
+		"folke/zen-mode.nvim",
+		keys = {
+			{ "<leader>uz", "<cmd>ZenMode<cr>", desc = "Zen Mode" },
+		},
+	},
+	{
+		"folke/twilight.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	-- Modify which-key keys
+	{
+		"folke/which-key.nvim",
+		opts = function()
+			require("which-key").add({
+				{ "<leader>z", group = "fold" },
+			})
+		end,
+	},
 }
